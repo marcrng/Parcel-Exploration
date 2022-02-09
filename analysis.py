8915c86b40594b8296c0520161cb24089d80c734
marcrng <59784362+marcrng@users.noreply.github.com>
2022-02-09 14:55:09 -0800

    Import parcel financial data

---
+++ analysis.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import matplotlib as plt
+
+df = pd.read_csv('Data/parcel_data_kcparcels.csv')
+
--- data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# Import parcel data csv from King County GIS
-parcel_data = pd.read_csv ('Data/KCParcels.csv')
-
-# Update sitetype
-# Create unique ID
-# Remove major/minor
-# Make 'nan' values to NULL
