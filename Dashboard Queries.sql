89e1ee5d7457b92bfd895d0271188cb6985d9e21
marcrng <59784362+marcrng@users.noreply.github.com>
2022-02-16 21:44:39 -0800

    Create custom query for dashboard to show detailed view of individual parcels

--- Dashboard Queries.sql
+++ Dashboard Queries.sql
@@ -1,6 +1,6 @@
 use parcel_data;
 
-# Selecting total property value of Single-Family residential grouped by zip
+# Selecting total property value divided by population of Single-Family residential grouped by zip
 create table tb_zip_total
 (
     cityname varchar(255),
@@ -19,3 +19,18 @@ insert into tb_zip_total (cityname, zip5, zip_total)
 select cityname, zip5
 from addresses
 group by cityname;
+
+select a.zip5, zip_total / population as ratio
+from addresses a
+join tb_zip_total tzt on a.zip5 = tzt.zip5
+join uszips u on a.zip5 = u.zip
+group by zip5;
+
+# Query for Parcel Valuations - Detail
+# Query execution was too slow without a custom query
+select parcel_num , objectid, name, cityname, address, description, val_total
+from kcparcels k
+join addresses a on k.OBJECTID = a.object_id
+join geodata g on k.OBJECTID = g.object_id
+join site_types st on k.SITETYPE = st.id
+join details d on k.OBJECTID = d.object_id
